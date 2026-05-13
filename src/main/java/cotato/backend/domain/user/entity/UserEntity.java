2e76f3385a12e841297d983e4640390e90fef902
Cuyqo <saon0410@gmail.com>
2026-05-14 00:25:12 +0900

    feat: 사용자 엔티티 추가

--- src/main/java/cotato/backend/domain/user/entity/UserEntity.java
+++ src/main/java/cotato/backend/domain/user/entity/UserEntity.java
@@ -1,4 +1,34 @@
 package cotato.backend.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+@Entity
+@Table(name= "user")
+@NoArgsConstructor(access = PROTECTED)
+@Getter
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String age;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Enumerated(STRING)
+    private UserType userType;
+
+    @Enumerated(STRING)
+    private Role staffRole;
 }
